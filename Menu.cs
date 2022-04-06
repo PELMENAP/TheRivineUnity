9b07830763dbb1c6f7182aa675c4b9f4c2bbc573
PELMENAP <92215614+PELMENAP@users.noreply.github.com>
2022-04-06 21:47:02 +0300

    Update Menu.cs
    bez komentariev

--- Menu.cs
+++ Menu.cs
@@ -1,28 +1,67 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject settings;
+    
+    private bool _isLoading;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        settings.SetActive(false);
+    }
     public void StartGame()
     {
-        DontDestroyOnLoad(this);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (_isLoading)
+        {
+            return;
+        }
+        InterfaceMainGame.normkey = true;
+        StartCoroutine(LoadScene());
     }
-
     public void LoadGame()
     {
-        DontDestroyOnLoad(this);
-        PlayerController con = new PlayerController();
-        SaveLoad.Load(ref con.data);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (_isLoading)
+        {
+            return;
+        }
+        InterfaceMainGame.loadkey = true;
+        StartCoroutine(LoadScene());
     }
+
+    private IEnumerator LoadScene()
+    {
+        _isLoading = true;
+        bool waitFading = true;
+        Fader.instance.FadeIn(() => waitFading = false);
+        while (waitFading)
+        {
+            yield return null;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        waitFading = true;
+        Fader.instance.FadeOut(() => waitFading = false);
+
+        while (waitFading)
+        {
+            yield return null;
+        }
+        _isLoading = false;
+        Destroy(Fader.instance.gameObject);
+        Destroy(this.gameObject);
+    }
+
+    public void MoveMenu(){
+        menu.SetActive(settings.activeInHierarchy);
+        settings.SetActive(!menu.activeInHierarchy); 
+    }
+
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void Exit()
-    {
-        SceneManager.LoadScene(0);
     }
 }
