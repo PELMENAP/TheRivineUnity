5d2f3711931193be696b7c6a7bad2f5323817f12
PELMENAP <92215614+PELMENAP@users.noreply.github.com>
2022-04-06 21:45:56 +0300

    Update DayCycle.cs
    ny za eto srazy laik

--- DayCycle.cs
+++ DayCycle.cs
@@ -1,25 +1,41 @@
 using UnityEngine;
+
 [ExecuteInEditMode]
 public class DayCycle : MonoBehaviour
 {
-    [SerializeField] Gradient dirlightGradient;
-    [SerializeField] Gradient ambientlightGradient;
+    public static bool isday = true;
+    [SerializeField] Gradient sunGradient;
+    [SerializeField] private Transform player;
+    [SerializeField, Range(1, 3600)] private int dayinSecond = 60;
+    [SerializeField, Range(0f, 1f)] private float time;
+    [SerializeField, Range(1, 200)] private int radius;
+    [SerializeField, Range(1, 10)] private int speed = 3;
+    [SerializeField] private UnityEngine.Rendering.Universal.Light2D globalLight;
+    private UnityEngine.Rendering.Universal.Light2D sun;
+    
+    private void Awake(){
+        sun = this.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+    }
 
-    [SerializeField, Range(1, 3600)] float dayinSecond = 60;
-    [SerializeField, Range(0f, 1f)] float timeProgress;
-    [SerializeField] Light dirLight;
-
-    void Update()
+    private void Update()
     {
         if (Application.isPlaying)
         {
-            timeProgress += Time.deltaTime / dayinSecond;
+            time += Time.deltaTime / dayinSecond;
+            sun.shadowIntensity = (-2.4f * time * time + 2.4f * time) / 2;
         }
-        if (timeProgress > 1f)
+        if (time > 1f)
         {
-            timeProgress = 0f;
+            time = 0f;
         }
-        dirLight.color = dirlightGradient.Evaluate(timeProgress);
-        RenderSettings.ambientLight = ambientlightGradient.Evaluate(timeProgress);
+        sun.color = globalLight.color = sunGradient.Evaluate(time);
+        if(time >= 0.2f && time <= 0.8f){
+            sun.transform.position = new Vector2(-Mathf.Cos((time - 0.2f) / 0.6f * speed) * radius, Mathf.Sin((time - 0.2f) / 0.6f  * speed) * radius) + new Vector2(player.position.x, player.position.y);
+            sun.intensity = globalLight.intensity = (- 280 / 9 * time * time + 280 / 9 * time - 43 / 9 - 0.8f) / 4;
+            isday = true;
+        }
+        else{
+            isday = false;
+        }
     }
 }
